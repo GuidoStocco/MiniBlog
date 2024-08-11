af1f1b823af35f489547caa01ae366091d7d8b81
unknown <guidostoccodev@gmail.com>
2024-08-11 11:54:29 -0300

    register form

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -4,7 +4,28 @@ import styles from './Register.module.css';
 const Register = () => {
     return(
         <div>
-            <h1>Register</h1>
+            <h1>Registre-se</h1>
+            <form>
+                <label>
+                    <span>Nome:</span>
+                    <input type="text" name='displayName' placeholder='Digite seu nome' required/>
+                </label>
+
+                <label>
+                    <span>E-mail:</span>
+                    <input type="email" name='email' placeholder='Digite seu email' required/>
+                </label>
+
+                <label>
+                    <span>Senha:</span>
+                    <input type="password" name='password' placeholder='Digite sua senha' required/>
+                </label>
+
+                <label>
+                    <span>Confirmação de senha:</span>
+                    <input type="password" name='confirmPassword' placeholder='Confirme sua senha' required/>
+                </label>
+            </form>
         </div>
     )
 };
